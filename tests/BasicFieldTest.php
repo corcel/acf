6bd33770208ac33299e673e607cbad8888532711
Junior Grossi <juniorgro@gmail.com>
2016-10-04 19:03:05 -0300

    Tests passing for the textarea field

--- tests/BasicFieldTest.php
+++ tests/BasicFieldTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Corcel\Acf\Field\Basic\Text;
+use Corcel\Acf\Field\Basic\Textarea;
 use Corcel\Post;
 
 /**
@@ -10,10 +11,28 @@ use Corcel\Post;
  */
 class BasicFieldTest extends PHPUnit_Framework_TestCase 
 {
+    /**
+     * @var Post
+     */
+    protected $post;
+
+    /**
+     * Setup a base $this->post object to represent the page with the basic fields
+     */
+    protected function setUp()
+    {
+        $this->post = Post::find(11); // it' a page with the custom fields
+    }
+
     public function testTextFieldValue()
     {
-        $post = Post::find(11); // it' a page with the custom fields
-        $text = new Text($post, 'fake_text');
+        $text = new Text($this->post, 'fake_text');
         $this->assertEquals('Proin eget tortor risus', $text->get());
+    }
+
+    public function testTextareaFieldValue()
+    {
+        $text = new Textarea($this->post, 'fake_textarea');
+        $this->assertEquals('Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.', $text->get());
     }
 }

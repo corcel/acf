fb0bcfab300810129d5a027d06671b07fa5290ca
Junior Grossi <juniorgro@gmail.com>
2016-10-04 18:43:44 -0300

    Add basic phpunit structure

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$capsule = \Corcel\Database::connect($params = [
+    'database' => 'corcel_acf',
+    'username' => 'root',
+    'password' => '',
+    'host' => '127.0.0.1',
+]);

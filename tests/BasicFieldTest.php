1ffafcb0a5a89bea15b1149d3c1e44c699493c15
Junior Grossi <juniorgro@gmail.com>
2016-10-04 19:08:54 -0300

    Tests passing for password field

--- tests/BasicFieldTest.php
+++ tests/BasicFieldTest.php
@@ -2,6 +2,7 @@
 
 use Corcel\Acf\Field\Basic\Email;
 use Corcel\Acf\Field\Basic\Number;
+use Corcel\Acf\Field\Basic\Password;
 use Corcel\Acf\Field\Basic\Text;
 use Corcel\Acf\Field\Basic\Textarea;
 use Corcel\Acf\Field\Basic\Url;
@@ -56,5 +57,11 @@ class BasicFieldTest extends PHPUnit_Framework_TestCase
         $url = new Url($this->post, 'fake_url');
         $this->assertEquals('https://corcel.org', $url->get());
 
+    }
+
+    public function testPasswordFieldValue()
+    {
+        $password = new Password($this->post, 'fake_password');
+        $this->assertEquals('123change', $password->get());
     }
 }

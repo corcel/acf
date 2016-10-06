488ec77511aadbc98013c9a34d978f5e8902a622
Junior Grossi <juniorgro@gmail.com>
2016-10-06 12:05:46 -0300

    Make all tests for factory working

--- tests/FieldFactoryTest.php
+++ tests/FieldFactoryTest.php
@@ -14,14 +14,14 @@ class FieldFactoryTest extends PHPUnit_Framework_TestCase
     public function testTextField()
     {
         $post = Post::find(11);
-        $text = FieldFactory::make('text', $post, 'fake_text');
+        $text = FieldFactory::make('fake_text', $post);
         $this->assertEquals('Proin eget tortor risus', $text);
     }
 
     public function testTextareaField()
     {
         $post = Post::find(11);
-        $textarea = FieldFactory::make('textarea', $post, 'fake_textarea');
+        $textarea = FieldFactory::make('fake_textarea', $post);
         $this->assertTrue(is_string($textarea));
         $this->assertTrue(strlen($textarea) > 0);
     }
@@ -29,49 +29,49 @@ class FieldFactoryTest extends PHPUnit_Framework_TestCase
     public function testNumberField()
     {
         $post = Post::find(11);
-        $number = FieldFactory::make('number', $post, 'fake_number');
+        $number = FieldFactory::make('fake_number', $post);
         $this->assertTrue(is_numeric($number));
     }
 
     public function testEmailField()
     {
         $post = Post::find(11);
-        $email = FieldFactory::make('email', $post, 'fake_email');
+        $email = FieldFactory::make('fake_email', $post);
         $this->assertEquals('junior@corcel.org', $email);
     }
 
     public function testUrlField()
     {
         $post = Post::find(11);
-        $url = FieldFactory::make('url', $post, 'fake_url');
+        $url = FieldFactory::make('fake_url', $post);
         $this->assertEquals('https://corcel.org', $url);
     }
 
     public function testPasswordField()
     {
         $post = Post::find(11);
-        $password = FieldFactory::make('password', $post, 'fake_password');
+        $password = FieldFactory::make('fake_password', $post);
         $this->assertEquals('123change', $password);
     }
 
     public function testEditorField()
     {
         $post = Post::find(21);
-        $editor = FieldFactory::make('wysiwyg', $post, 'fake_editor');
+        $editor = FieldFactory::make('fake_editor', $post);
         $this->assertNotFalse(strpos($editor, '<em>'));
     }
 
     public function testOembedField()
     {
         $post = Post::find(21);
-        $embed = FieldFactory::make('oembed', $post, 'fake_oembed');
+        $embed = FieldFactory::make('fake_oembed', $post);
         $this->assertNotFalse(strpos($embed, 'youtube.com'));
     }
 
     public function testImageField()
     {
         $post = Post::find(21);
-        $image = FieldFactory::make('image', $post, 'fake_image');
+        $image = FieldFactory::make('fake_image', $post);
         $this->assertTrue(is_numeric($image->width));
         $this->assertTrue(is_numeric($image->height));
         $this->assertNotFalse(strpos($image->url, 'http'));

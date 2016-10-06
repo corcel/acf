669f90da547e3bf9cb4c8fc8cfdcc92cb50bb809
Junior Grossi <juniorgro@gmail.com>
2016-10-06 10:32:14 -0300

    Change basic tests to the new form of fetching values

--- tests/BasicFieldsTest.php
+++ tests/BasicFieldsTest.php
@@ -25,40 +25,40 @@ class BasicFieldsTest extends PHPUnit_Framework_TestCase
 
     public function testTextFieldValue()
     {
-        $text = new Text($this->post, 'fake_text');
-        $text->build();
+        $text = new Text();
+        $text->process('fake_text', $this->post);
 
         $this->assertEquals('Proin eget tortor risus', $text->get());
     }
 
     public function testTextareaFieldValue()
     {
-        $textarea = new Text($this->post, 'fake_textarea');
-        $textarea->build();
+        $textarea = new Text();
+        $textarea->process('fake_textarea', $this->post);
 
         $this->assertEquals('Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.', $textarea->get());
     }
 
     public function testNumberFieldValue()
     {
-        $number = new Text($this->post, 'fake_number');
-        $number->build();
+        $number = new Text();
+        $number->process('fake_number', $this->post);
 
         $this->assertEquals('1984', $number->get());
     }
 
     public function testEmailFieldValue()
     {
-        $email = new Text($this->post, 'fake_email');
-        $email->build();
+        $email = new Text();
+        $email->process('fake_email', $this->post);
 
         $this->assertEquals('junior@corcel.org', $email->get());
     }
 
     public function testUrlFieldValue()
     {
-        $url = new Text($this->post, 'fake_url');
-        $url->build();
+        $url = new Text();
+        $url->process('fake_url', $this->post);
 
         $this->assertEquals('https://corcel.org', $url->get());
 
@@ -66,8 +66,8 @@ class BasicFieldsTest extends PHPUnit_Framework_TestCase
 
     public function testPasswordFieldValue()
     {
-        $password = new Text($this->post, 'fake_password');
-        $password->build();
+        $password = new Text();
+        $password->process('fake_password', $this->post);
 
         $this->assertEquals('123change', $password->get());
     }

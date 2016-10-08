e9a132840a89d664a8ee1a6c1e3626f0fec8195e
Junior Grossi <juniorgro@gmail.com>
2016-10-07 22:56:59 -0300

    Add tests for date picker, date time picker and time picker fields

--- tests/JqueryFieldsTest.php
+++ tests/JqueryFieldsTest.php
@@ -26,4 +26,18 @@ class JqueryFieldsTests extends PHPUnit_Framework_TestCase
         $date->process('fake_date_picker', $this->post);
         $this->assertEquals('10/13/2016', $date->get()->format('m/d/Y'));
     }
+
+    public function testDateTimePickerField()
+    {
+        $dateTime = new DateTime();
+        $dateTime->process('fake_date_time_picker', $this->post);
+        $this->assertEquals('05:06:08/19-10:2016', $dateTime->get()->format('s:i:H/d-m:Y')); // 2016-10-19 08:06:05
+    }
+
+    public function testTimePickerField()
+    {
+        $time = new DateTime();
+        $time->process('fake_time_picker', $this->post);
+        $this->assertEquals('00/17/30', $time->get()->format('s/H/i')); // 17:30:00
+    }
 }

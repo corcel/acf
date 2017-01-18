4b509e89877350a6b8d5d85368e1f649ba304bc0
Junior Grossi <juniorgro@gmail.com>
2017-01-18 19:19:27 -0200

    Format code

--- src/FieldFactory.php
+++ src/FieldFactory.php
@@ -24,88 +24,88 @@ use Illuminate\Support\Collection;
  */
 class FieldFactory
 {
-    private function __construct()
-    {
-    }
-
-    /**
-     * @param string      $name
-     * @param Post        $post
-     * @param null|string $type
-     *
-     * @return FieldInterface|Collection|string
-     */
-    public static function make($name, Post $post, $type = null)
-    {
-        if (null === $type) {
-            $fakeText = new Text($post);
-	        $key = $fakeText->fetchFieldKey($name);
+	private function __construct()
+	{
+	}
 	
-	        if ($key === null) { // Field does not exist
-                return null;
-            }
-	
-	        $type = $fakeText->fetchFieldType($key);
-        }
-	
-	    switch ($type) {
-            case 'text':
-            case 'textarea':
-            case 'number':
-            case 'email':
-            case 'url':
-            case 'password':
-            case 'wysiwyg':
-            case 'editor':
-            case 'oembed':
-            case 'embed':
-            case 'color_picker':
-            case 'select':
-            case 'checkbox':
-            case 'radio':
-                $field = new Text($post);
-                break;
-            case 'image':
-            case 'img':
-                $field = new Image($post);
-                break;
-            case 'file':
-                $field = new File($post);
-                break;
-            case 'gallery':
-                $field = new Gallery($post);
-                break;
-            case 'true_false':
-            case 'boolean':
-                $field = new Boolean($post);
-                break;
-            case 'post_object':
-            case 'post':
-            case 'relationship':
-                $field = new PostObject($post);
-                break;
-            case 'page_link':
-                $field = new PageLink($post);
-                break;
-            case 'taxonomy':
-            case 'term':
-                $field = new Term($post);
-                break;
-            case 'user':
-                $field = new User($post);
-                break;
-            case 'date_picker':
-            case 'date_time_picker':
-            case 'time_picker':
-                $field = new DateTime($post);
-                break;
-            case 'repeater':
-                $field = new Repeater($post);
-                break;
-        }
-
-        $field->process($name);
-
-        return $field;
-    }
+	/**
+	 * @param string $name
+	 * @param Post $post
+	 * @param null|string $type
+	 *
+	 * @return FieldInterface|Collection|string
+	 */
+	public static function make($name, Post $post, $type = null)
+	{
+		if (null === $type) {
+			$fakeText = new Text($post);
+			$key = $fakeText->fetchFieldKey($name);
+			
+			if ($key === null) { // Field does not exist
+				return null;
+			}
+			
+			$type = $fakeText->fetchFieldType($key);
+		}
+		
+		switch ($type) {
+			case 'text':
+			case 'textarea':
+			case 'number':
+			case 'email':
+			case 'url':
+			case 'password':
+			case 'wysiwyg':
+			case 'editor':
+			case 'oembed':
+			case 'embed':
+			case 'color_picker':
+			case 'select':
+			case 'checkbox':
+			case 'radio':
+				$field = new Text($post);
+				break;
+			case 'image':
+			case 'img':
+				$field = new Image($post);
+				break;
+			case 'file':
+				$field = new File($post);
+				break;
+			case 'gallery':
+				$field = new Gallery($post);
+				break;
+			case 'true_false':
+			case 'boolean':
+				$field = new Boolean($post);
+				break;
+			case 'post_object':
+			case 'post':
+			case 'relationship':
+				$field = new PostObject($post);
+				break;
+			case 'page_link':
+				$field = new PageLink($post);
+				break;
+			case 'taxonomy':
+			case 'term':
+				$field = new Term($post);
+				break;
+			case 'user':
+				$field = new User($post);
+				break;
+			case 'date_picker':
+			case 'date_time_picker':
+			case 'time_picker':
+				$field = new DateTime($post);
+				break;
+			case 'repeater':
+				$field = new Repeater($post);
+				break;
+		}
+		
+		$field->process($name);
+		
+		return $field;
+	}
 }

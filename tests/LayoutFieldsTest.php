6ef8397906c9eca0082e01592d38127bdb8fb711
Marco Boom <marco@quest-media.nl>
2017-02-09 09:04:06 +0100

    Add tests for Flexible Content field

--- tests/LayoutFieldsTest.php
+++ tests/LayoutFieldsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Corcel\Acf\Field\Repeater;
+use Corcel\Acf\Field\FlexibleContent;
 
 /**
  * Class LayoutFieldsTest.
@@ -29,10 +30,33 @@ class LayoutFieldsTest extends PHPUnit_Framework_TestCase
         $repeater->process('fake_repeater_2');
         $fields = $repeater->get()->toArray();
 
+
         $this->assertEquals('admin', $fields[0]['fake_user']->nickname);
         $this->assertEquals('admin', $fields[1]['fake_user']->nickname);
         $this->assertEquals(2, $fields[0]['fake_relationship']->count());
         $this->assertEquals(1, $fields[1]['fake_relationship']->count());
-        $this->assertInstanceOf(Post::class, $fields[0]['fake_relationship']->first());
+        $this->assertInstanceOf('Corcel\Post', $fields[0]['fake_relationship']->first());
+    }
+
+
+    public function testFlexibleContentField()
+    {
+        $page = Post::find(86);
+        $flex = new FlexibleContent($page);
+        $flex->process('fake_flexible_content');
+        $layout = $flex->get();
+
+        $this->assertEquals(3, $layout->count());
+
+		$this->assertEquals('normal_text', $layout[0]->type);
+		$this->assertEquals('Lorem ipsum', $layout[0]->fields->text);
+
+		$this->assertEquals('related_post', $layout[1]->type);
+		$this->assertInstanceOf(Post::class, $layout[1]->fields->post);
+
+		$this->assertEquals('multiple_posts', $layout[2]->type);
+		$this->assertEquals(2, $layout[2]->fields->post->count());
+		$this->assertInstanceOf('Corcel\Post', $layout[2]->fields->post->first());
+
     }
 }

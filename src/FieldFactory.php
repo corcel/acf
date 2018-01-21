3536234088d4d97afada484d20c6dc316ddf79d3
Naeem <naeem_ins@hotmail.com>
2018-01-21 12:14:39 +0400

    Google Map field
    Google map field included which will return array with index 'address', 'lat', 'lng'

--- src/FieldFactory.php
+++ src/FieldFactory.php
@@ -107,6 +107,9 @@ class FieldFactory
             case 'flexible_content':
                 $field = new FlexibleContent($post);
                 break;
+			case 'google_map':
+                $field = new GoogleMap($post);
+                break;
             default: return null;
         }
 
